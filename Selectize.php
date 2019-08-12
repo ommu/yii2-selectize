8495b491429ce6022aa933c4e71fd7f34e907748
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-08-12 17:31:06 +0700

    modify author

--- Selectize.php
+++ Selectize.php
@@ -6,7 +6,7 @@
  * For more details and usage information on Selectize, see the [guide article on Selectize](guide:selectize).
  * @see yii2mod\selectize\Selectize
  * 
- * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @author Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
  * @created date 29 April 2019, 12:28 WIB
